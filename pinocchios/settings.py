94a229868647d2909be6d68cbdb12744e6fa246e
James L Barnett <james_barnett@fas.harvard.edu>
2018-07-29 02:19:05 +0000

    adds css file into static

--- pinocchios/settings.py
+++ pinocchios/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
